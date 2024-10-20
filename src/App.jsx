de2cf10bac216e4669ed5649c3d76cf8f0eb1754
Immortals430 <vishalkumar619430@gmail.com>
2024-10-20 15:47:40 +0530

    scrollbar changed, loading added

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,48 @@
-import { useEffect, useState } from 'react'
-import Navbar from './Component/Navbar'
-import Head from './Component/Head'
-import About from './Component/About'
-import Experience from './Component/Experience'
-import MyWorks from './Component/MajorProject'
-import OtherWorks from './Component/OtherWorks'
-import Contact from './Component/Contact'
-import LeftFixedComp from './Component/LeftFixedComp'
-import RightFixedComp from './Component/RightFixedComp'
+import Navbar from "./Component/Navbar";
+import Head from "./Component/Head";
+import About from "./Component/About";
+import Experience from "./Component/Experience";
+import MyWorks from "./Component/MajorProject";
+import OtherWorks from "./Component/OtherWorks";
+import Contact from "./Component/Contact";
+import LeftFixedComp from "./Component/LeftFixedComp";
+import RightFixedComp from "./Component/RightFixedComp";
+import { Atom } from "react-loading-indicators";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
 
   return (
     <>
-    <Navbar />
-    <main>
+      {loading ? (
+        <div className="loading">
+        <Atom color={"#6ce3d3"} size="medium" />
+        </div>
 
-    <Head />
-    <About />
-    <Experience />
-    <MyWorks />
-    <OtherWorks />
-    <Contact />
-
-    </main>
-    <LeftFixedComp />
-    <RightFixedComp />
+      ) : (
+        <>
+          <Navbar />
+          <main>
+            <Head />
+            <About />
+            <Experience />
+            <MyWorks />
+            <OtherWorks />
+            <Contact />
+          </main>
+          <LeftFixedComp />
+          <RightFixedComp />
+        </>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
